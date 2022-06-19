894d72464a8c25f6bf787e0a274951b02a528b17
utsmannn <utsmannn@gmail.com>
2022-06-19 15:43:46 +0700

    add env database url

---
+++ README.md
@@ -0,0 +1,8 @@
+
+### Deploy ke heroku
+    - pake github action
+### Log heroku
+```
+heroku logs --tail -a demo-aej 
+// demo-aej is app name heroku
+```
--- src/main/kotlin/com/aej/demo/database/DatabaseComponent.kt
+++ src/main/kotlin/com/aej/demo/database/DatabaseComponent.kt
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Component
 @Component
 class DatabaseComponent {
 
-    companion object {
-        private const val DB_URL = "mongodb+srv://aej:1234@cluster0.npggr.mongodb.net/?retryWrites=true&w=majority"
-    }
+    private val databaseURL = System.getenv("DATABASE_URL")
 
-    val database: MongoClient = KMongo.createClient(DB_URL)
+    val database: MongoClient = KMongo.createClient(databaseURL)
 }
